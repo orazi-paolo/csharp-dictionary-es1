397e204f80a1c82906a18e8197095b53628c47f8
orazi-paolo <orazi.paolo10@gmail.com>
2024-12-29 20:25:27 +0100

    first commit

---
+++ csharp-dictionary-es1/Program.cs
@@ -0,0 +1,16 @@
+﻿// dichiaro la variabile dictionary e la inizializzo con un nuovo oggetto Dictionary<string, int>
+var dictionary = new Dictionary<string, int>();
+// aggiungo elementi al dizionario
+dictionary.Add("Mario", 25);
+dictionary.Add("Luca", 22);
+dictionary.Add("Sara", 24);
+
+// rimuovo un elemento dal dizionario
+dictionary.Remove("Luca");
+
+// stampo il contenuto del dizionario aggiornato
+Console.WriteLine("Studenti e le loro età:");
+foreach (var item in dictionary)
+{
+    Console.WriteLine($"{item.Key}: {item.Value}");
+}
